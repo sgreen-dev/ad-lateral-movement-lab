30816d4e11000d69315271eabfda2e086b58e747
Shurron Green <shurron@protonmail.com>
2026-05-11 00:40:41 -0400

    Apply terraform fmt to cost-controls module

--- terraform/modules/cost-controls/main.tf
+++ terraform/modules/cost-controls/main.tf
@@ -78,20 +78,20 @@ resource "aws_budgets_budget" "monthly" {
 
   # Alert at 80% of actual spend
   notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 80
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "ACTUAL"
-    subscriber_sns_topic_arns  = [aws_sns_topic.alerts.arn]
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 80
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "ACTUAL"
+    subscriber_sns_topic_arns = [aws_sns_topic.alerts.arn]
   }
 
   # Alert at 100% forecasted (gives you a heads-up before you hit the cap)
   notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 100
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "FORECASTED"
-    subscriber_sns_topic_arns  = [aws_sns_topic.alerts.arn]
+    comparison_operator       = "GREATER_THAN"
+    threshold                 = 100
+    threshold_type            = "PERCENTAGE"
+    notification_type         = "FORECASTED"
+    subscriber_sns_topic_arns = [aws_sns_topic.alerts.arn]
   }
 }
 
